a3683ceb4a7f3d2a29e26650580733adf2699c1a
Anders Andersson <git@panic.nu>
2018-10-09 21:12:28 +0200

    Make script more portable and updated for more Payara agnostic script

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -d -p 8080:8080 -p 4848:4848 --name anders -v /Users/megaman/java/practicejavaee8/ear/target:/opt/payara41/deployments payara/server-full
+docker run --rm -d -p 8080:8080 -p 4848:4848 --name anders -v $(pwd)/ear/target:/opt/payara/deployments payara/server-full
