b493389b217478e5c5890c08b272cb1b8fe8f581
Anders Andersson <git@panic.nu>
2018-10-09 21:52:48 +0200

    Move ear file to use autodeploy in Payara 5, also rename container

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -d -p 8080:8080 -p 4848:4848 --name anders -v $(pwd)/ear/target:/opt/payara/deployments payara/server-full
+docker run --rm -d -p 8080:8080 -p 4848:4848 --name javaee8 -v $(pwd)/ear/target/ear-1.0.0-SNAPSHOT.ear:/opt/payara5/glassfish/domains/domain1/autodeploy/ear.ear payara/server-full
